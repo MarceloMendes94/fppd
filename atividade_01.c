0dcc986f4f0f1ca41ef854a6d7d5516463933c28
notebook pessoal <marcelocelularxiaomi@gmail.com>
2022-08-17 09:05:58 -0300

    FEAT somatório multithread genérico.

--- atividade_01.c
+++ atividade_01.c
@@ -5,8 +5,8 @@
 #include<stdlib.h>
 #include<time.h>
 
-#define SIZE 100000
-#define N_THREADS 4
+#define SIZE 900000
+#define N_THREADS 2
 
 typedef struct 
 {
@@ -18,7 +18,12 @@ typedef struct
 
 void* funcao_thread (void* args){
     dataframe_t *df_t = ((dataframe_t *) args);
-    printf("%d\n",df_t->id);
+    printf("Thread em execução(%d)...\n",df_t->id);
+    int inicio = (SIZE/N_THREADS)* (df_t->id-1);
+    int fim = (SIZE/N_THREADS)* (df_t->id);
+    for(int i=inicio;i<fim;i++){
+        df_t->soma = df_t->soma + df_t->p_vetor[i];
+    }
 }
 
 
@@ -36,20 +41,29 @@ int main(){
     clock_t m_2 = clock();
     double Tempo = (m_2 - m_1) * 1000.0 / CLOCKS_PER_SEC;
     printf("Soma iterativa %ld\n", soma);
-    printf("Tempo gasto: %g ms.", Tempo);
+    printf("Tempo gasto: %g ms\n", Tempo);
     
     // Com Threads
-    pthread_t tid;
-    dataframe_t df;
-    df.id=1;
-    df.soma=0;
-    df.p_vetor = vetor; 
+    pthread_t lst_thread[N_THREADS];
+    dataframe_t lst_df[N_THREADS];
 
-    pthread_create(&tid,NULL, funcao_thread, &df);
-    pthread_join(tid, NULL);   
+    clock_t m_3 = clock();  
+    for(int i=0;i<N_THREADS;i++){
+        lst_df[i].id = i+1;
+        lst_df[i].soma = 0;
+        lst_df[i].p_vetor = vetor;
+        pthread_create(&lst_thread[i],NULL, funcao_thread, &lst_df[i]);
+    }
+    long int soma_multi = 0;
+    for(int i=0;i<N_THREADS;i++){
+        pthread_join(lst_thread[i], NULL); 
+        soma_multi = soma_multi + lst_df[i].soma;
+    }
 
+    printf("soma multhread some é:%ld\n",soma_multi);
+    clock_t m_4 = clock();
+    double Tempo_2 = (m_4 - m_3) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Tempo gasto: %g ms\n", Tempo_2);
     system("read -p 'Press Enter to continue...' var");
-    
-    
     return 0;
 }
