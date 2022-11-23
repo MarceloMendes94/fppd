8406a00335ab64f703ffaf433e68fbb2a85eaf6b
Marcelo <marcelo.mendes@cliconsultoria.com.br>
2022-11-22 21:50:07 -0300

    Lista 2 iniciada

--- lista_02.c
+++ lista_02.c
@@ -4,8 +4,8 @@
 #include<stdlib.h>
 #include<pthread.h>
 
-#define vetor_size 32
-#define num_threads 4
+#define vetor_size 10000
+#define num_threads 10
 
 
 typedef struct data_t{
@@ -17,46 +17,53 @@ typedef struct data_t{
     int* vetor;
 }data_t;
 
-void* thread_func(void* arg){
+
+
+void* thread_func_2(void* arg){
     data_t* pkg = (data_t*)arg;
-    int local_index;
+    int* local_pos;
     do {
         pthread_mutex_lock(pkg->mutex);
-        printf("mutou\n");
-        local_index = *pkg->pos;
-        pkg->pos++;
-        pthread_mutex_unlock(pkg->mutex);
-        if (local_index< vetor_size){
-            pkg->soma_parcial += pkg->vetor[local_index]; 
-        }
-    } while(local_index< vetor_size);
-
-    //pthread_mutex_lock(&mutex1);
-    //sum += partial_sum;
-    //pthread_mutex_unlock(&mutex1);
+        local_pos = pkg->pos;
+        (*pkg->pos)++;    
+        //pthread_mutex_unlock(pkg->mutex);
+        
+        if((*local_pos) <= vetor_size){
+           // pthread_mutex_lock(pkg->mutex);
+            pkg->soma_parcial = pkg->soma_parcial+ pkg->vetor[*local_pos];
+            pkg->qte_somada++;
+            pthread_mutex_unlock(pkg->mutex);
+        }       
+        
+    } while((*local_pos) <=vetor_size);    
 }
+
+
+
 
 int main(){
     int i;
-    int vetor[vetor_size];
-    int* pos = (int*) malloc(1*sizeof(int)); 
+    
+    int vetor[vetor_size+1];
+    for( i=0 ; i<vetor_size+1 ; i++){
+        vetor[i] = i;
+    }
+
     pthread_t* lst_thread = (pthread_t*)malloc(num_threads*sizeof(pthread_t));
     data_t* lst_data = (data_t*)malloc(num_threads * sizeof(data_t));
+    
     pthread_mutex_t* lst_mutex = (pthread_mutex_t*)malloc(num_threads * sizeof(pthread_mutex_t));
-
-    *pos = 0;
-
-    for( i=0 ; i<vetor_size ; i++){
-        vetor[i] = i+1;
-    }
     // iniciando os mutexes
     for(i=0; i<num_threads ;i++){
         pthread_mutex_init(&lst_mutex[i],NULL);
-    }
+    } 
+
+    int* pos = (int*) malloc(1*sizeof(int)); 
+    *pos = 0;
 
     //prenchendo array de dataframe
     for(i=0 ; i<num_threads ; i++){
-        lst_data[i].id = 0;
+        lst_data[i].id = i;
         lst_data[i].qte_somada = 0;
         lst_data[i].soma_parcial = 0;
         lst_data[i].pos = pos;
@@ -64,16 +71,30 @@ int main(){
         lst_data[i].mutex = &lst_mutex[i];
     }
 
+ 
+    // cria as thread
     for(i=0; i<num_threads ;i++){
-        pthread_create(&lst_thread[i] , NULL , thread_func , &lst_data[i]);
+        pthread_create(&lst_thread[i] , NULL , thread_func_2 , &lst_data[i]);
     }
-
+    //junta os resultados
     for(i=0; i<num_threads ;i++){
         pthread_join(lst_thread[i] , NULL);
     }
     
-    int total = 0;
-    for(i<0;i<num_threads;i++){
-       printf("%d",lst_data[i].soma_parcial);
+    for(i=0 ; i<num_threads ; i++){
+        printf("id:%d| qte:%d | soma:%d\n",lst_data[i].id,lst_data[i].qte_somada , lst_data[i].soma_parcial);
     }
+    int total=0;
+    for(i=0 ; i<num_threads ; i++){
+        total = total+ lst_data[i].qte_somada;
+        
+    }
+    printf("---------\ntotal:%d\n",total); 
+
+    free(lst_thread);
+    free(lst_data);
+    free(lst_mutex);
+    free(pos);
+
+    return 0;
 }
